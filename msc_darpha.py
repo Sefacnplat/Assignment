7a649bd3e790b9fb193e59af3094bc70552324f4
Sefacnplat <149816646+Sefacnplat@users.noreply.github.com>
2023-11-23 22:39:13 +0300

    msc_darpha.py

--- msc_darpha.py
+++ msc_darpha.py
@@ -1,12 +1,20 @@
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, classification_report
 
+#Load DARPA data set
+dataset_path = 'x.csv'
+df = pd.read_csv(dataset_path)
+
+# Define properties and tags
+X = df.drop('label', axis=1)
+y = df['label']
+
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Standardize the features (important for SVM)
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
@@ -14,10 +22,8 @@ X_test_scaled = scaler.transform(X_test)
 # Initialize the SVM classifier
 svm_classifier = SVC(kernel='linear', C=1)
 
-# Train the SVM model
 svm_classifier.fit(X_train_scaled, y_train)
 
-# Make predictions on the test set
 y_pred = svm_classifier.predict(X_test_scaled)
 
 # Evaluate the model performance
