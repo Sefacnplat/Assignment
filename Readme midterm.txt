b48ea35c24bdbd83311e9b3a785108a9ef44b866
Sefacnplat <149816646+Sefacnplat@users.noreply.github.com>
2023-11-23 22:21:16 +0300

    Add files via upload
    For midterm

---
+++ Readme midterm.txt
@@ -0,0 +1,23 @@
+# Security Threat Detection with Data Mining
+
+## Introduction
+
+Machine learning frameworks and libraries are used when applying data mining techniques for security threat detection. Let's use the Support Vector Machine (SVM) classifier in supervised machine learning in this example.
+In this example, depending on our dataset and needs, we may need to explore different algorithms, perform hyperparameter tuning, and conduct more extensive preprocessing. Additionally, techniques such as clustering or anomaly detection algorithms can be used for unsupervised learning.
+
+## Getting Started
+
+### Prerequisites
+
+- Python 3.x
+- Required libraries: pandas, scikit-learn
+
+Install the required libraries using:
+
+```bash
+pip install pandas scikit-learn
+
+After please Replace your_dataset.csv with the actual path to your DARPA dataset CSV file.
+
+Run the Python script:
+python threat_detection.py
---
+++ msc_darpha.py
@@ -0,0 +1,41 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, classification_report
+
+# Assuming 'X' is your feature matrix, and 'y' is your target variable (labels)
+# Replace 'X' and 'y' with your actual variable names
+
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Standardize the features (important for SVM)
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# Initialize the SVM classifier
+svm_classifier = SVC(kernel='linear', C=1)
+
+# Train the SVM model
+svm_classifier.fit(X_train_scaled, y_train)
+
+# Make predictions on the test set
+y_pred = svm_classifier.predict(X_test_scaled)
+
+# Evaluate the model performance
+accuracy = accuracy_score(y_test, y_pred)
+classification_report_result = classification_report(y_test, y_pred)
+
+# Print the results
+print(f"Accuracy: {accuracy:.2f}")
+print("Classification Report:")
+print(classification_report_result)
